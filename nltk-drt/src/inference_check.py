f0388843a028618167c252c29e9aea4b247ae5ba
Peter.Makarov.Russia <Peter.Makarov.Russia@97b1e69d-ddcf-9723-e12d-7734815f13e3>
2010-08-23 13:08:50 +0000

    Slightly changed comments in temporaldrt.py. Resolve() looks good. inference_check.py contains a preview of the inference tools available to us and a list of the issues we need to sort out.

---
+++ nltk-drt/src/inference_check.py
@@ -0,0 +1,54 @@
+from nltk.inference.prover9 import Prover9Command
+from nltk.inference.mace import MaceCommand
+from nltk import load_parser
+from temporaldrt import DrtParser
+from nltk.sem.drt import AbstractDrs
+
+import nltkfixtemporal
+
+def gr(s, recursive=False):
+    return []
+
+AbstractDrs.get_refs = gr
+
+"""
+As Mace looks for a minimal solution, those are the things we need to have so that 
+models make sense:
+
+    (i) the basic types distinction is lost in Mace. It needs to be reintroduced
+     in the background so that the following DRS-conditions are added into the DRSs
+     in which a referent of a certain basic type appears: e.g. DRS([x,e,t],[]) should,
+     at the point the inference tools are called, carry conditions
+     DRS([x,e.t],[individual(x),event(e),time(t)]).
+     
+    (ii) at each call of the inference tools, the following meaning postulate should
+     be added all x.((individual(x) -> -(eventuality(x) | time(x))) & (eventuality(t) ->
+     -time(x)) & (state(x) -> (eventuality(x) & -event(x))) & (event(x) -> eventuality(x))).
+     
+    (iii) John in John(x) is treated as a predicate; either we write in our grammars
+     the way B&B2 do John=x or make it turn into John=x e.g. when the fol() method is called. 
+     
+    (iv) presupposition that all individuals mentioned in a discourse are uniquely
+     and disjointly identified by their names. This could be done with following
+     DRS-condition: DRS([x,y],[Bill(x),Claire(y)]) turns into 
+     DRS([x,y],[x=Bill, y=Claire, -(x=y)]). In other words, for each pair of proper name
+     carrying individuals x and y, it should be stated in a DRS from which both are
+     acceptable that -(x=y). This could be added when the inference tools are called.
+"""
+
+if __name__ == "__main__":
+    parser = load_parser('file:../data/tenseaspect.fcfg', logic_parser=DrtParser())
+    trees = parser.nbest_parse('Angus does not own a dog'.split())
+    expr = trees[0].node['SEM'].resolve()
+    parse = expr.fol()
+    
+    print "Temporal DRT Expression: %s \n" % parse
+    
+    m = MaceCommand(None, [parse])
+    print "Mace: %s \n\n %s \n" % (m.build_model(), m.valuation) 
+    
+    p = Prover9Command(parse, []).prove()
+    print "Prover9: %s: %s" % (parse, p)
+    
+    expr.draw()
+            
