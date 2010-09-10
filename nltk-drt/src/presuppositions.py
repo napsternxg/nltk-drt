c7649643205f263430a9cdfdc2f8064da69d8fe1
emma.li.tuebingen@gmail.com <emma.li.tuebingen@gmail.com@97b1e69d-ddcf-9723-e12d-7734815f13e3>
2010-09-10 20:29:57 +0000

    temporaldrt: fixed replace() presuppositions.py: Proper names - seems to work. Run to test it with sentences_2, test2

--- nltk-drt/src/presuppositions.py
+++ nltk-drt/src/presuppositions.py
@@ -1,8 +1,8 @@
 import temporaldrt
 import nltk.sem.drt as drt
 import deepcopy_readings
-from deepcopy_readings import PresuppositionDRS, DRS
-from temporaldrt import DrtProperNameApplicationExpression
+from deepcopy_readings import Readings
+from temporaldrt import DrtProperNameApplicationExpression, PresuppositionDRS, DRS
 
 # Nltk fix
 import nltkfixtemporal
@@ -18,8 +18,8 @@ class DrtTokens(temporaldrt.DrtTokens):
     PERSONAL_PRONOUN_DRS = 'PERSPRON'
     POSSESSIVE_PRONOUN_DRS = 'POSSPRON'
     REFLEXIVE_PRONOUN_DRS = 'REFLPRON'
-    PRESUPPOSITION_DRS = [PROPER_NAME_DRS, DEFINITE_DESCRIPTION_DRS, 
-                          PERSONAL_PRONOUN_DRS, POSSESSIVE_PRONOUN_DRS, 
+    PRESUPPOSITION_DRS = [PROPER_NAME_DRS, DEFINITE_DESCRIPTION_DRS,
+                          PERSONAL_PRONOUN_DRS, POSSESSIVE_PRONOUN_DRS,
                           REFLEXIVE_PRONOUN_DRS]
 
 class DrtParser(temporaldrt.DrtParser):
@@ -34,7 +34,7 @@ class DrtParser(temporaldrt.DrtParser):
         """Parse new types of DRS: presuppositon DRSs.
         """
         self.assertNextToken(DrtTokens.OPEN)
-        drs = self.handle_DRS(tok,context)
+        drs = self.handle_DRS(tok, context)
         if tok == DrtTokens.PROPER_NAME_DRS:
             return ProperNameDRS(drs.refs, drs.conds)
         elif tok == DrtTokens.DEFINITE_DESCRIPTION_DRS:
@@ -51,12 +51,16 @@ class DrtParser(temporaldrt.DrtParser):
         return DRS(drs.refs, drs.conds)
 
 class ProperNameDRS(PresuppositionDRS):
+    def __init__(self, refs, conds):
+        print "Proper name", refs, conds
+        super(ProperNameDRS, self).__init__(refs, conds)
     def readings(self, trail=[]):
         """A proper name always yields one reading: it is either global binding 
         or global accommodation (if binding is not possible)"""
         # In DRSs representing sentences like 'John, who owns a dog, feeds it',
         # there will be more than one condition in the presupposition DRS.
-        # Find the proper name application expression
+        # Find the proper name application expression.
+        print "PROPER NAME READINGs"
         proper_name = None
         for cond in self.conds:
             if isinstance(cond, DrtProperNameApplicationExpression):
@@ -69,6 +73,7 @@ class ProperNameDRS(PresuppositionDRS):
         ########
         antecedent_ref = None
         outer_drs = drss['global']
+        local_drs = drss.get('local', outer_drs)
         for cond in outer_drs.conds:
             # Binding is only possible if there is a condition with the 
             # same functor (proper name) at the global level
@@ -78,19 +83,38 @@ class ProperNameDRS(PresuppositionDRS):
                 break
         if antecedent_ref:
             # 1) Put all conditions (except the proper name itself)
-            # into the outer DRS, and replace the referent in them with
+            # into the outer DRS, and replace the proper name referent in them with
             # antecedent_ref.
             # 2) In the conditions of the local DRS, replace the 
-            # referent of proper name with antecedent_ref.
-            def function():
-                pass
-            
-        
-        
-        
-        # TODO: if you generate a reading/readings, return at once!
-        # And all the drss above return them, too.
-        pass
+            # referent of the proper name with antecedent_ref.
+            def outer_binding(drs):
+                """Put all conditions from the presupposition DRS
+                (except the proper name itself) into the outer DRS, 
+                and replace the proper name referent in them with antecedent_ref"""
+                self.replace(proper_name.argument.variable, antecedent_ref, True)
+                drs.conds.extend([cond for cond in self.conds if cond is not proper_name])
+            def inner(drs):
+                """In the conditions of the local DRS, replace the 
+                referent of the proper name with antecedent_ref"""
+                if local_drs is outer_drs: outer_binding(drs)
+                drs.replace(proper_name.argument.variable, antecedent_ref, True)
+                drs.conds.remove(self)
+            # Return the reading
+            return [{outer_drs: outer_binding,
+                     local_drs: inner}]
+        # If no suitable antecedent has been found in the outer DRS,
+        # binding is not possible, so we go for accommodation instead.
+        def outer_accommodation(drs):
+            """Accommodation: put all referents and conditions from 
+            the presupposition DRS into the outer DRS"""
+            print "outer_accommodation"
+            drs.refs.extend(self.refs) 
+            drs.conds.extend(self.conds)
+        def inner_remove(drs):
+            if local_drs is outer_drs: outer_accommodation(drs)
+            drs.conds.remove(self)
+        return [{outer_drs: outer_accommodation,
+                 local_drs: inner_remove}]
 
 class DefiniteDescriptionDRS(PresuppositionDRS):
     def readings(self, trail=[]):
@@ -114,7 +138,7 @@ class ReflexivePronounDRS(PronounDRS):
 #################################
 ## Demo
 #################################
-def test (sentence, parser, draworig = True, drawdrs=True):
+def test (sentence, parser, draworig=True, drawdrs=True):
     print '==========================', sentence , '=========================='
     if isinstance(sentence, list):
         tree = parser.nbest_parse(sentence.pop(0).split())[0].node['SEM']
@@ -135,6 +159,20 @@ def test (sentence, parser, draworig = True, drawdrs=True):
     #print a
     #print "\n\n"
     if drawdrs: a.draw()
+
+def test_2(sentence, parser):
+    print '==========================', sentence , '=========================='
+    if isinstance(sentence, list):
+        tree = parser.nbest_parse(sentence.pop(0).split())[0].node['SEM']
+        while sentence:
+            sent = sentence.pop(0)
+            tree_new = parser.nbest_parse(sent.split())[0].node['SEM']
+            tree = tree + tree_new
+    else: 
+        tree = parser.nbest_parse(sentence.split())[0].node['SEM']
+    a = tree.simplify()
+    a.draw()
+    for reading in Readings(tree): reading.draw()
     
 if __name__ == '__main__':
     
@@ -147,20 +185,13 @@ if __name__ == '__main__':
                  'A boy marries the girl',
                  'The boy marries the girl',
                  'Every boy marries the girl',
-                 ['A boy marries a girl','He walks'], # no presuppositions here
+                 ['A boy marries a girl', 'He walks'], # no presuppositions here
                  'The boy s father marries his girl',
-                 ['John marries his girl','He walks'],
+                 ['John marries his girl', 'He walks'],
                  'The boy walks',
                  ['A boy marries the girl', 'He walks'],
-                 'A boy s father marries his girl'
+                 'A boy s father marries his girl',
                  ]
-    """
-    # A DRS sees the referents of the immediately embedded 
-    # presuppositional DRSs as its referents
-    p = DrtParser().parse
-    expr = p(r'\x.DRS([],[DEFDESCR([x],[boy(x)])walk(x)])(z)')
-    expr_1 = p(r'DRS([],[DEFDESCR([x],[boy(x)])walk(x)])')
-    print expr.simplify()
-    print expr_1.free()
-    """
-    for sentence in sentences[2:3]: test(sentence, parser, draworig = True, drawdrs = True)
+    sentences_2 = ['John walks', 'Every girl marries John']
+    #for sentence in sentences[2:3]: test(sentence, parser, draworig = True, drawdrs = True)
+    for sentence in sentences_2[:]: test_2(sentence, parser)
