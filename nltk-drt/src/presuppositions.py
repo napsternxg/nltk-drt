73e167e667a6541730f61d02d9f79919d151f59f
emma.li.tuebingen@gmail.com <emma.li.tuebingen@gmail.com@97b1e69d-ddcf-9723-e12d-7734815f13e3>
2010-09-14 22:06:05 +0000

    add remover if deleteme is true

--- nltk-drt/src/presuppositions.py
+++ nltk-drt/src/presuppositions.py
@@ -1,3 +1,6 @@
+""" !!! The latest working version is presuppositions v.114 + temporaldrt v.114.
+In presuppositions v.114, fix the bug in test by replacing tree with a""" 
+
 import temporaldrt
 import nltk.sem.drt as drt
 from temporaldrt import PresuppositionDRS, DRS, DrtTokens, DrtApplicationExpression, Reading
@@ -31,11 +34,11 @@ class DrtParser(temporaldrt.DrtParser):
 class ProperNameDRS(PresuppositionDRS):
     
     class OuterBinding(object):
-        def __init__(self, presupp_drs, proper_name, antecedent_ref, cond_index):
+        def __init__(self, presupp_drs, proper_name, antecedent_ref, condition_index):
             self.presupp_drs = presupp_drs
             self.proper_name = proper_name
             self.antecedent_ref = antecedent_ref
-            self.cond_index = cond_index
+            self.condition_index = condition_index
         def __call__(self, drs):
             """Put all conditions from the presupposition DRS
             (except the proper name itself) into the outer DRS, 
@@ -48,40 +51,41 @@ class ProperNameDRS(PresuppositionDRS):
             conds_to_move = [cond for cond in newdrs.conds \
                             if cond.function.variable.name != self.proper_name.function.variable.name]
             # Put the conditions at the position of the original presupposition DRS
-            if self.cond_index is None: # it is an index, it can be zero
+            if self.condition_index is None: # it is an index, it can be zero
                 drs.conds.extend(conds_to_move)
             else:
-                drs.conds = drs.conds[:self.cond_index+1]+conds_to_move+drs.conds[self.cond_index+1:]
-                return drs
+                drs.conds = drs.conds[:self.condition_index+1]+conds_to_move+drs.conds[self.condition_index+1:]
+            return drs
             
     class InnerReplace(object):
-        def __init__(self, proper_name, antecedent_ref, local_is_outer, cond_index):
+        def __init__(self, presupp_drs, proper_name, antecedent_ref, local_is_outer, condition_index):
+            self.presupp_drs = presupp_drs
             self.proper_name = proper_name
             self.antecedent_ref = antecedent_ref
             self.local_is_outer = local_is_outer
-            self.cond_index = cond_index
+            self.condition_index = condition_index
         def __call__(self, drs):
                 """In the conditions of the local DRS, replace the 
                 referent of the proper name with antecedent_ref"""
                 if self.local_is_outer:
-                    func = ProperNameDRS.OuterBinding(drs, self.proper_name, self.antecedent_ref, self.cond_index)
+                    func = ProperNameDRS.OuterBinding(self.presupp_drs, self.proper_name, self.antecedent_ref, self.condition_index)
                     drs = func(drs)
                 return drs.replace(self.proper_name.argument.variable, self.antecedent_ref, True)
     
     class OuterAccommodation(object):
-        def __init__(self, presupp_drs, cond_index):
+        def __init__(self, presupp_drs, condition_index):
             self.presupp_drs = presupp_drs
-            self.cond_index = cond_index
+            self.condition_index = condition_index
         def __call__(self, drs):
             """Accommodation: put all referents and conditions from 
             the presupposition DRS into the outer DRS"""
             drs.refs.extend(self.presupp_drs.refs) 
-            if self.cond_index is None:
+            if self.condition_index is None:
                 drs.conds.extend(self.presupp_drs.conds)
             else:
-                print self.cond_index
+                print self.condition_index
                 print "outer accommodation drs.conds before",drs.conds
-                drs.conds = drs.conds[:self.cond_index+1]+self.presupp_drs.conds+drs.conds[self.cond_index+1:]
+                drs.conds = drs.conds[:self.condition_index+1]+self.presupp_drs.conds+drs.conds[self.condition_index+1:]
                 print "outer accommodation drs.conds after",drs.conds
             return drs
         
@@ -111,18 +115,18 @@ class ProperNameDRS(PresuppositionDRS):
             and cond.function.variable.name == proper_name.function.variable.name:
                 antecedent_ref = cond.argument
                 break
-        cond_index = None
+        condition_index = None
         if local_drs is outer_drs:
-            cond_index = local_drs.conds.index(self)
+            condition_index = local_drs.conds.index(self)
         if antecedent_ref:           
             # Return the reading
             if local_drs is outer_drs:
-                return [Reading([(local_drs, ProperNameDRS.InnerReplace(proper_name, antecedent_ref, (local_drs is outer_drs), cond_index))])]
-            return [Reading([(outer_drs, ProperNameDRS.OuterBinding(self, proper_name, antecedent_ref, cond_index)),
-                     (local_drs, ProperNameDRS.InnerReplace(proper_name, antecedent_ref, (local_drs is outer_drs), cond_index))])]
+                return [Reading([(local_drs, ProperNameDRS.InnerReplace(self, proper_name, antecedent_ref, (local_drs is outer_drs), condition_index))])], True
+            return [Reading([(outer_drs, ProperNameDRS.OuterBinding(self, proper_name, antecedent_ref, condition_index)),
+                     (local_drs, ProperNameDRS.InnerReplace(self, proper_name, antecedent_ref, (local_drs is outer_drs), condition_index))])], True
         # If no suitable antecedent has been found in the outer DRS,
         # binding is not possible, so we go for accommodation instead.
-        return [Reading([(outer_drs, ProperNameDRS.OuterAccommodation(self, cond_index))])]
+        return [Reading([(outer_drs, ProperNameDRS.OuterAccommodation(self, condition_index))])], True
     
 class DefiniteDescriptionDRS(PresuppositionDRS):
     def _presupposition_readings(self, trail=[]):
