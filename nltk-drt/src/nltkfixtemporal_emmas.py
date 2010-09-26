9efb7d35e396045ce747483e6ec6816a73167373
emma.li.tuebingen <emma.li.tuebingen@gmail.com>
2010-09-26 14:17:52 +0000

    Definite descriptions use nltk's wordnet for ontologies. Tested a bit, but not much. More detailed tests will follow soon.

---
+++ nltk-drt/src/nltkfixtemporal_emmas.py
@@ -0,0 +1,58 @@
+from presuppositions import DRS, DrtLambdaExpression, DrtApplicationExpression, DrtAbstractVariableExpression, DrtNegatedExpression, DrsDrawer
+from nltk.sem.logic import BinaryExpression
+
+"""
+This is the same file as nltkfixtemporal, but 
+I use it with presuppositions.py and presuppositions_test.py
+"""
+#from nltk.sem.drt import AbstractDrs
+#
+#def gr(s, recursive=False):
+#    print "I am %s, %s" % (s, type(s))
+#    return []
+#
+#AbstractDrs.get_refs = gr
+
+def _handle(self, expression, command, x=0, y=0):
+    """
+    @param expression: the expression to handle
+    @param command: the function to apply, either _draw_command or _visit_command
+    @param x: the top of the current drawing area
+    @param y: the left side of the current drawing area
+    @return: the bottom-rightmost point
+    """
+    if command == self._visit_command:
+        #if we don't need to draw the item, then we can use the cached values
+        try:
+            #attempt to retrieve cached values
+            right = expression._drawing_width + x
+            bottom = expression._drawing_height + y
+            return (right, bottom)
+        except AttributeError:
+            #the values have not been cached yet, so compute them
+            pass
+    
+    if isinstance(expression, DrtAbstractVariableExpression):
+        factory = self._handle_VariableExpression
+    elif isinstance(expression, DRS):
+        factory = self._handle_DRS
+    elif isinstance(expression, DrtNegatedExpression):
+        factory = self._handle_NegatedExpression
+    elif isinstance(expression, DrtLambdaExpression):
+        factory = self._handle_LambdaExpression
+    elif isinstance(expression, BinaryExpression):
+        factory = self._handle_BinaryExpression
+    elif isinstance(expression, DrtApplicationExpression):
+        factory = self._handle_ApplicationExpression
+    else:
+        raise Exception, expression.__class__.__name__
+        
+    (right, bottom) = factory(expression, command, x, y)
+    
+    #cache the values
+    expression._drawing_width = right - x
+    expression._drawing_height = bottom - y
+        
+    return (right, bottom)
+
+DrsDrawer._handle = _handle
